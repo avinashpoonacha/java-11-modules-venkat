1b6c03989fc49e771135c7f202312cc9d8ca4b5d
venkats@agiledeveloper.com <venkats@agiledeveloper.com>
2021-05-25 06:43:33 -0600

    versions of java and the number of modules

---
+++ code/part1/examples/02_versions_of_java_and_the_number_of_modules/Sample.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class Sample {
+  public static void main(String[] args) {
+   /*
+     java --list-modules
+
+     java --list-modules | more
+
+     java --list-modules | wc -l
+
+     version and the number of modules in the JRE
+     9   78...
+     10  72...
+     11  71, 72 
+     13  70
+     15  69, 70
+     16  71, 70, 67
+   */
+  }
+}
