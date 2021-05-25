143f4e798de2a3a3d734091517ed762bd59a41bb
venkats@agiledeveloper.com <venkats@agiledeveloper.com>
2021-05-25 07:44:37 -0600

    public is not public

---
+++ code/part1/examples/07_public_is_not_public/build.sh
@@ -0,0 +1,21 @@
+/bin/rm -rf output
+
+mkdir -p output/mlib
+
+mkdir -p output/classes
+javac -d output/classes `find first -name *.java`
+jar -c -f output/mlib/first.jar -C output/classes .
+/bin/rm -rf output/classes
+
+mkdir -p output/classes
+javac -d output/classes -p output/mlib `find second -name *.java`
+jar -c -f output/mlib/second.jar -C output/classes .
+/bin/rm -rf output/classes
+
+java -p output/mlib \
+  -m com.agiledeveloper.thesecond/com.agiledeveloper.second.Second
+
+
+echo "public is no longer public"
+echo "public + exports is public"
+
