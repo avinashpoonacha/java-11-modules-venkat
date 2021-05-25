8729b210805d502d9e106b7ed75a90bbd1dd2c86
venkats@agiledeveloper.com <venkats@agiledeveloper.com>
2021-05-25 08:18:16 -0600

    third depends on first directly

---
+++ code/part2/examples/02_third_depends_on_first_directly/third/com/agiledeveloper/third/Third.java
@@ -0,0 +1,19 @@
+package com.agiledeveloper.third;
+
+import com.agiledeveloper.first.First;
+import com.agiledeveloper.second.Second;
+
+public class Third {
+  public static void main(String[] args) {
+    System.out.println("this is Third from " + Third.class.getModule());
+
+    System.out.println(new Second());
+
+    System.out.println(new First());
+  }
+}
+
+//thethird -> thesecond -> thefirst
+//thethird -> thefirst
+//Third is able to use First
+
